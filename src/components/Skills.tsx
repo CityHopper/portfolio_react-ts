6547e656267378ca4c14b9a43dd4cdc6810bfe37
young <peacestrokes@gmail.com>
2022-09-07 11:34:17 +0900

    Add Career component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,37 +4,45 @@ function Skills () {
     interface skill {
         name: string;
         level: number;
+        item: Array<string>;
     }
 
     let skillList: Array<skill> = [
-        {name: "Front-end", level: 90},
-        {name: "Back-end", level: 60},
-        {name: "React.js", level: 60}
+        {name: "Front-end", level: 90, item:["React", "Redux", "HTML5", "SCSS", "CSS3", "ES6+", "TypeScript"]},
+        {name: "Back-end", level: 60, item:["Flask", "AWS", "Django", "SQL"]},
+        {name: "Data Science", level: 50, item:["Python", "Pandas", "Numpy", "NLP"]},
+        {name: "Co-work", level: 70, item: ["Git (GitHub/GitLab)", "Zeplin", "Slack"]}
     ]
 
     return (
         <article className={"container"}>
-            <section className={"container__half"}>
-                <h1>Skills</h1>
+            <section className={"container__whole"}>
+                <h1>Skills & Qualification</h1>
                 <p>
                     I create successful responsive websites with up-to-date technologies.
-                    The main area of my expertise is front-end development, HTML, CSS, JS, and building small and medium web apps.
+                    The main area of my expertise is front-end development,
+                    HTML, CSS, JS, and building small and medium web apps.
                 </p>
                 <p>
-                    I also have a back-end experience,
-                    which I built an API by Flask in order to provide geographic data(GeoJson) on AWS by myself.
+                    Regarding back-end experience,
+                    had built an Flask API providing geographic data(GeoJson) at a start-up company by myself.
                 </p>
                 <p>
+                    I also have data analysis ability with Python(Pandas, numpy) and R.
                 </p>
             </section>
             <section className={"container__half"}>
                 <table className={"chart"}>
                 { skillList.map((i, index) => (
                     <tr>
-                        <span>{i.name}</span>
+                        <h4>{i.name}</h4>
                         <footer>
                             <div style={{width: `${i.level}%`}} />
                         </footer>
+                        {i.item.map((itm) => (
+                            <span className={""}>{itm}</span>
+                        ))}
+                        <hr/>
                     </tr>
                 ))
                 }
